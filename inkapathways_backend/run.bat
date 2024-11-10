693a6fd416e9ca6c549189f0b312452f6efd774e
AlvaroCuba <famousalvarocubaporras@gmail.com>
2024-11-10 17:05:35 -0500

    requerimientos nuevos/archivo_run

--- inkapathways_backend/run.bat
+++ inkapathways_backend/run.bat
@@ -7,8 +7,6 @@ if not exist venv (
     call venv\Scripts\activate
     echo Instalando dependencias...
     pip install -r requirements.txt
-    python manage.py makemigrations api_users api_lugares api_root modelo_v1
-    python manage.py migrate api_users api_lugares api_root modelo_v1
 
 ) else (
     echo Activando entorno virtual...
