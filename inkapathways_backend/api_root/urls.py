676f16c518d7aebe23879aec84cd3cda7a8faf8c
AlvaroCuba <famousalvarocubaporras@gmail.com>
2024-12-10 23:45:07 -0500

    url api_root

--- inkapathways_backend/api_root/urls.py
+++ inkapathways_backend/api_root/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import PreguntaList, RespuestaList, TripticoFinal , TripticoCreateAPIView ,TripticoDetailAPIView
 urlpatterns = [
-    path('preguntas/', PreguntaList.as_view(), name='pregunta-list'),
+    path('preguntas-totales/', PreguntaList.as_view(), name='pregunta-list'),
     path('respuestas-totales/', RespuestaList.as_view(), name='respuesta-list'),
     path('prediccion/', TripticoFinal.as_view(), name="prediccion"),
     path('triptico/', TripticoCreateAPIView.as_view(), name="Triptico"),
